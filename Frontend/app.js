b9f39719ac9a2355ec241a8dc0958614003d8095
RajveerSingh44222 <rajsisodia446@gmail.com>
2026-06-24 17:49:04 +0530

    added the backend url to codebase

--- Frontend/app.js
+++ Frontend/app.js
@@ -2,7 +2,7 @@
    VETRA PASSWORD MANAGER - APPLICATION LOGIC
    ========================================== */
 
-const API_BASE_URL = "http://localhost:8000/api/v1";
+const API_BASE_URL = "vetra-production-2545.up.railway.app";
 
 class VetraApp {
   constructor() {
